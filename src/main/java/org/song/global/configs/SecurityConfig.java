b22339224687d11f442af91feff1e29b28a71ac6
SongGeunHo <104110883+SongGunHo@users.noreply.github.com>
2025-08-07 11:23:28 +0900

    save

--- src/main/java/org/song/global/configs/SecurityConfig.java
+++ src/main/java/org/song/global/configs/SecurityConfig.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,6 +14,12 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.csrf(c->c.disable());
         return http.build();
     }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder(); // 비밀 번호 암호화
+    }
+
 }

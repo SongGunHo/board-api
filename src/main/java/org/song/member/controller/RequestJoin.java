e0304ed9c76815066e95ec581121a82371d8b54f
SongGeunHo <104110883+SongGunHo@users.noreply.github.com>
2025-08-07 09:46:29 +0900

    save

--- src/main/java/org/song/member/controller/RequestJoin.java
+++ src/main/java/org/song/member/controller/RequestJoin.java
@@ -2,6 +2,7 @@ package org.song.member.controller;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
@@ -10,11 +11,17 @@ public class RequestJoin {
     @NotBlank
     @Email
     private String email; // 이메일
-
+    @NotBlank
+    @Size(min=8)
     private String password; // 패스워드
+
+    @NotBlank
     private String confirmPassword; // 2차 패스워드
+    @NotBlank
     private String name; // 이름
+    @NotBlank // string 에서만 가능 하다
     private String mobile;// 전화번호
+
     private boolean termsAgree; // 약관 동의
 
 

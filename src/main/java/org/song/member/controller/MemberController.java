92a83dc73c4e7eef327fc32da654c54c37f8ac80
SongGeunHo <104110883+SongGunHo@users.noreply.github.com>
2025-08-07 11:21:58 +0900

    save

--- src/main/java/org/song/member/controller/MemberController.java
+++ src/main/java/org/song/member/controller/MemberController.java
@@ -2,12 +2,13 @@ package org.song.member.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.BadRequestException;
+import org.song.global.lib.Utis;
 import org.song.member.services.JoinService;
 import org.song.member.validators.JoinValidator;
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,10 +16,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberController {
     private final JoinService service;
     private final JoinValidator validator;
+    private final Utis utis;
 
     // 회원 가입
     @PostMapping
-    public void join(@Valid RequestJoin form , Errors errors){
-
+    @ResponseStatus(HttpStatus.CREATED) // 201 응답
+    public void join(@Valid @RequestBody RequestJoin form , Errors errors){
+        validator.validate(form, errors);
+        if(errors.hasErrors()){
+            throw new BadRequestException(utis.getErrorMessages(errors));
+        }
+        service.process(form);
     }
 }

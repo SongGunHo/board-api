a38402bbae9d4d0da08b0883929a1b04854b136f
SongGeunHo <104110883+SongGunHo@users.noreply.github.com>
2025-08-08 15:20:27 +0900

    test중

---
+++ src/main/java/org/song/file/controllers/RequestThumb.java
@@ -0,0 +1,12 @@
+package org.song.file.controllers;
+
+import lombok.Data;
+
+@Data
+public class RequestThumb {
+
+    private Long seq; // 파일 등록번호
+    private int width;
+    private int height;
+    private boolean crop; // 크롭 여부
+}
